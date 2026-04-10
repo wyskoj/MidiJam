9476d8e82e8b1bacbc53a2ef8a45b6b843999466
Jacob Wysko <jacob@wysko.org>
2026-04-10 19:51:16 -0400

    Fix 2003 build errors

--- instruments/Agogos.h
+++ instruments/Agogos.h
@@ -9,7 +9,6 @@
 #include "../model/Ms3dBundle.h"
 #include "instrument.h"
 
-#include <cstdint>
 #include <dmusici.h>
 
 // ---------------------------------------------------------------------------
