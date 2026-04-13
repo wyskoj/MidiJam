5dfbdb7eebfb53cc3ab25d047b28c971845f2501
Jacob Wysko <jacob@wysko.org>
2026-04-13 17:57:38 -0400

    Add side stick

--- instruments/percussion/SnareDrum.cpp
+++ instruments/percussion/SnareDrum.cpp
@@ -15,8 +15,11 @@ extern int g_percussion_time_queue[88][32];
 extern float g_recoil_snare;
 extern Ms3dBundle* g_drumSet_Stick_ms3d;
 extern float MAX_RECOIL;
+extern float g_recoil_sideStick;
 
 __int16 g_snareStickActive = 0;
+__int16 word_469B48[512];
+__int16 word_46919A;
 
 void UpdateSnareDrum() {
     GLfloat angle; // [esp+0h] [ebp-3Ch]
@@ -84,4 +87,67 @@ void UpdateSnareDrum() {
 }
 
 void UpdateSideStick() {
+    GLfloat angle; // [esp+0h] [ebp-30h]
+    GLfloat anglea; // [esp+0h] [ebp-30h]
+    __int16 i; // [esp+18h] [ebp-18h]
+    __int16 v3; // [esp+24h] [ebp-Ch]
+    __int16 v4; // [esp+28h] [ebp-8h]
+
+    v3 = 10000;
+    v4 = 0;
+    for ( i = 0; i < 32; ++i )
+    {
+        if ( word_469B48[i] > 0 && word_469B48[i] < v3 )
+        {
+            v3 = word_469B48[i];
+            ++v4;
+        }
+    }
+    if ( v3 <= 0 || (double)v3 >= 200.0 )
+    {
+        if ( g_recoil_sideStick <= 0.0 )
+        {
+            if ( v4 <= 0 )
+            {
+                g_snareStickActive = 0;
+            }
+            else if ( word_46919A == 1 )
+            {
+                glPushMatrix();
+                glRotatef(-75.0, 0.0, 1.0, 0.0);
+                glTranslatef(0.0, 0.27000001, 6.5999999);
+                glRotatef(25.0, 1.0, 0.0, 0.0);
+                glTranslatef(0.0, 0.0, -11.0);
+                glRotatef(180.0, 0.0, 1.0, 0.0);
+                g_drumSet_Stick_ms3d->RenderModel();
+                glPopMatrix();
+            }
+        }
+        else
+        {
+            word_46919A = 1;
+            glPushMatrix();
+            glRotatef(-75.0, 0.0, 1.0, 0.0);
+            glTranslatef(0.0, 0.27000001, 6.5999999);
+            anglea = (MAX_RECOIL - g_recoil_sideStick) / MAX_RECOIL * 25.0;
+            glRotatef(anglea, 1.0, 0.0, 0.0);
+            glTranslatef(0.0, 0.0, -11.0);
+            glRotatef(180.0, 0.0, 1.0, 0.0);
+            g_drumSet_Stick_ms3d->RenderModel();
+            glPopMatrix();
+        }
+    }
+    else
+    {
+        word_46919A = 1;
+        glPushMatrix();
+        glRotatef(-75.0, 0.0, 1.0, 0.0);
+        glTranslatef(0.0, 0.27000001, 6.5999999);
+        angle = (double)v3 / 200.0 * 25.0;
+        glRotatef(angle, 1.0, 0.0, 0.0);
+        glTranslatef(0.0, 0.0, -11.0);
+        glRotatef(180.0, 0.0, 1.0, 0.0);
+        g_drumSet_Stick_ms3d->RenderModel();
+        glPopMatrix();
+    }
 }
